12e9cc046fb79a32fbe5893e0895b6810bc36f0c
Ignacio-Us <i.uribe01@ufromail.cl>
2024-07-03 21:32:14 -0400

    Initial commit

---
+++ src/main/java/ufro/esptec/proyecto_final_esp_tec1/ProyectoFinalEspTec1Application.java
@@ -0,0 +1,13 @@
+package ufro.esptec.proyecto_final_esp_tec1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalEspTec1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalEspTec1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ufro/esptec/proyecto_final_esp_tec1/ProyectoFinalEspTec1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ufro.esptec.proyecto_final_esp_tec1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalEspTec1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
