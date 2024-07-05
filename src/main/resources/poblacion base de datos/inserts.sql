2abfec42809cf71b43ce462b642a2c4b84059d2a
Ignacio-Us <i.uribe01@ufromail.cl>
2024-07-04 20:26:21 -0400

    Create inserts.sql

---
+++ src/main/resources/poblacion base de datos/inserts.sql
@@ -0,0 +1,13 @@
+INSERT INTO usuario (id, nombre, email, contrasena) VALUES
+(1, 'pepe', 'pepe@gmail.com', 12345),
+(2, 'juan', 'juan@gmail.com', 23456);
+
+INSERT INTO puntaje (id, puntaje, usuario_id) VALUES
+('1', 50, '2'),
+('2', 65, '2'),
+('3', 25, '1'),
+('4', 140, '1'),
+('5', 90, '1'),
+('6', 60, '2'),
+('7', 70, '1'),
+('8', 70, '2');
